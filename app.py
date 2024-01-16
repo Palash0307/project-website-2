73990794467e05a2ec876616b169f2356460ff93
Palash0307 <palashaggawral@gmail.com>
2024-01-16 17:04:38 +0530

    added HTML and Css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app =Flask(__name__)
 # @ decorater /-path to be deployed(right now its a home page)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
