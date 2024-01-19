108b582f6bbf615d6a248bf2de3ae18719a2c800
Palash0307 <palashaggawral@gmail.com>
2024-01-19 14:46:28 +0530

    security and database

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 
 from sqlalchemy import create_engine,text
 import os
-db_connection_string=os.environ('db_connection_string')
+
+db_connection_string = os.environ['db_connection']
 engine = create_engine(db_connection_string,
-                    connect_args={"ssl": {
+                    connect_args={"ssl": { #to get from clouddb sources
                                             "ssl_ca":"/etc/ssl/cert.pem",
                                             
                                         }
@@ -15,6 +16,6 @@ def load_jobs_from_db():
         result=conn.execute(text("Select * from jobs"))
         jobs=[]
         for row in result.all():
-            #cinverting legacy row into dicts
+            #converting legacy row into dicts
             jobs.append(row._mapping)
         return jobs
