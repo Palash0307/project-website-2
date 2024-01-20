7ab2389269ad8dea2ef25c2aacc08f06a7c9f30e
Palash0307 <palashaggawral@gmail.com>
2024-01-20 10:51:09 +0530

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask,render_template,jsonify
 from sqlalchemy import text
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db
 app =Flask(__name__)
 jobsdb=load_jobs_from_db()
 # @ decorater /-path to be deployed(right now its a home page)
 @app.route("/")
 def hello():
-    return render_template('home.html',jobs=jobsdb,company_name='Palash')
+    return render_template('home.html',jobs=jobsdb)
 @app.route("/api/jobs")
 def list_jobs():
     return jsonify(jobsdb)
+@app.route("/job/<id>")
+def show_job(id):
+    job=load_job_from_db(id)
+    if not job:
+        return "Not Found",404
+    # will extract the data from job and will put in jobpage.html
+    return render_template('jobpage.html',job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 
+#19th jan
 from sqlalchemy import create_engine,text
 import os
 #added security to protect the password
-db_connection_string = os.environ['db_connection']
+db_connection_string = os.environ['db_connection'] #name to be added in environment 19thjan
 engine = create_engine(db_connection_string,
                     connect_args={"ssl": { #to get from clouddb sources
                                             "ssl_ca":"/etc/ssl/cert.pem",
@@ -18,4 +19,20 @@ def load_jobs_from_db():
         for row in result.all():
             #converting legacy row into dicts
             jobs.append(row._mapping)
+            
         return jobs
+#to execute a specific job details
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        #val to show the id to be placed
+        result=conn.execute(text("Select * from jobs where id = :val").bindparams(val=id))
+        row = result.fetchone()
+        columns = ['id', 'title', 'location', 'salary','currency','responsibility','requirements']
+         # Access columns using names or indices, tuple doesn't take string values to map
+        values = [row[i] for i in range(len(columns))]
+       
+        
+        
+        return(values)
+        
+        
